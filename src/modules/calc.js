357bd3b6da78b4d585ab1f43218e00d752a1b77b
Vinare <scrat-me@mail.ru>
2022-03-09 05:17:22 +0300

    Refactored

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,13 +1,13 @@
 const calc = () => {
-  const inputNumber = document.querySelectorAll(".calc-block input");
+  const inputNumber = document.querySelectorAll(".calc-block input")
 
-  const regExpNumber = /[^0-9]+/i;
+  const regExpNumber = /[^0-9]+/i
 
   inputNumber.forEach((input) => {
     input.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(regExpNumber, "");
-    });
-  });
+      e.target.value = e.target.value.replace(regExpNumber, "")
+    })
+  })
 }
 
 export default calc
--- src/modules/validations.js
+++ src/modules/validations.js
@@ -1,55 +1,55 @@
 const validations = () => {
-  const inputMessage = document.getElementById("form2-message");
-  const inputText = document.querySelectorAll("form input[type='text']");
-  const inputEmail = document.querySelectorAll("form input[type='email']");
-  const inputPhone = document.querySelectorAll("form input[type='tel']");
+  const inputMessage = document.getElementById("form2-message")
+  const inputText = document.querySelectorAll("form input[type='text']")
+  const inputEmail = document.querySelectorAll("form input[type='email']")
+  const inputPhone = document.querySelectorAll("form input[type='tel']")
 
-  const regExpText = /[^а-я\-\s]+/i;
-  const regExpEmail = /[^a-z\@\-\_\.\!\~\*\']+/i;
-  const regExpPhone = /[^0-9\(\)\-]+/i;
+  const regExpText = /[^а-я\-\s]+/i
+  const regExpEmail = /[^a-z\@\-\_\.\!\~\*\']+/i
+  const regExpPhone = /[^0-9\(\)\-]+/i
 
-  const manyDash = /(\-+)/g;
-  const manySpace = /(\s+)/g;
-  const edgeDash = /^\-|\-$/g;
-  const edgeSpace = /^\s+|\s+$/g;
+  const manyDash = /(\-+)/g
+  const manySpace = /(\s+)/g
+  const edgeDash = /^\-|\-$/g
+  const edgeSpace = /^\s+|\s+$/g
 
   inputText.forEach((input) => {
     input.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(regExpText, "");
-    });
-  });
+      e.target.value = e.target.value.replace(regExpText, "")
+    })
+  })
 
   inputEmail.forEach((input) => {
     input.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(regExpEmail, "");
-    });
-  });
+      e.target.value = e.target.value.replace(regExpEmail, "")
+    })
+  })
 
   inputPhone.forEach((input) => {
     input.addEventListener("input", (e) => {
-      e.target.value = e.target.value.replace(regExpPhone, "");
-    });
-  });
+      e.target.value = e.target.value.replace(regExpPhone, "")
+    })
+  })
 
   inputMessage.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(regExpText, "");
-  });
+    e.target.value = e.target.value.replace(regExpText, "")
+  })
 
   inputText.forEach((input) => {
     input.addEventListener("blur", (e) => {
-      let capitalizeLetterFunc = (match) => match.toUpperCase();
+      let capitalizeLetterFunc = (match) => match.toUpperCase()
 
       e.target.value = e.target.value
         .replace(manyDash, "-")
         .replace(manySpace, " ")
         .replace(edgeDash, "")
-        .replace(edgeSpace, "");
+        .replace(edgeSpace, "")
       
-      e.target.value = e.target.value.toLowerCase();
+      e.target.value = e.target.value.toLowerCase()
       e.target.value = e.target.value
-      .replace(/^[а-я]{1}|\s{1}[а-я]{1}/gi, capitalizeLetterFunc);
-    });
-  });
+      .replace(/^[а-я]{1}|\s{1}[а-я]{1}/gi, capitalizeLetterFunc)
+    })
+  })
 
   inputEmail.forEach((input) => {
     input.addEventListener("blur", (e) => {
@@ -57,9 +57,9 @@ const validations = () => {
         .replace(manyDash, "-")
         .replace(manySpace, " ")
         .replace(edgeDash, "")
-        .replace(edgeSpace, "");
-    });
-  });
+        .replace(edgeSpace, "")
+    })
+  })
 
   inputPhone.forEach((input) => {
     input.addEventListener("blur", (e) => {
@@ -67,9 +67,9 @@ const validations = () => {
         .replace(manyDash, "-")
         .replace(manySpace, " ")
         .replace(edgeDash, "")
-        .replace(edgeSpace, "");
-    });
-  });
-};
+        .replace(edgeSpace, "")
+    })
+  })
+}
 
-export default validations;
+export default validations
