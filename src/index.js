06a315f20c585625a64382da4e5c44b6a9d77ef0
Vinare <scrat-me@mail.ru>
2022-03-09 05:17:42 +0300

    Refactored

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import modal from './modules/modal'
 import calc from './modules/calc'
 import validations from './modules/validations'
 
-timer('20 march 2022')
+timer('28 march 2022')
 menu()
 modal()
 calc()
