af9603326f5998ec03de4177c2f074e929b82440
Vinare <scrat-me@mail.ru>
2022-03-11 23:53:40 +0300

    1-st

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import modal from './modules/modal'
 import calc from './modules/calc'
 import validations from './modules/validations'
 import smoothScroll from './modules/smoothScroll'
+import tabs from './modules/tabs'
 
-timer('28 march 2022')
+timer('26 march 2022')
 menu()
 modal()
 calc()
 validations()
 smoothScroll()
+tabs()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,7 +5,7 @@ import {
 const modal = () => {
   const modal = document.querySelector('.popup')
   const buttons = document.querySelectorAll('.popup-btn')
-  const closeBtn = modal.querySelector('.popup-close')
+
   // изначально модальное окно opacity = 0
 
   buttons.forEach(btn => {
@@ -30,8 +30,10 @@ const modal = () => {
     })
   })
 
-  closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none'
+  modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+      modal.style.display = 'none'
+    }
   })
 }
 
