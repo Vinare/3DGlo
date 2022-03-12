40fd3155f4c54973fda556bdaf0a4bb5b54f8069
Vinare <scrat-me@mail.ru>
2022-03-12 15:50:23 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import calc from './modules/calc'
 import validations from './modules/validations'
 import smoothScroll from './modules/smoothScroll'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('26 march 2022')
 menu()
@@ -13,3 +14,4 @@ calc()
 validations()
 smoothScroll()
 tabs()
+slider()
